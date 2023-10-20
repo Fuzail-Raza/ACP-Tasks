c6b04b0ab21ba5040b6c84e08d3148cc9de111c8
Fuzail Raza <fuzailraza161@gmail.com>
2023-10-20 23:02:43 +0500

    Added all Tasks

--- lab1Task.java
+++ lab1Task.java
@@ -2,6 +2,60 @@ import java.util.*;
 
 public class lab1Task {
     
+    public static void primeNumber() {
+
+        int i,m=0,flag=0;
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter any Number");
+        int num=in.nextInt();
+        m=num/2;
+        if(num==0 || num==1){
+            System.out.println(num + " i s not a prime number");
+        }
+        else{
+            for(i=2;i<=m;i++){
+            if(num%i==0){
+                System.out.println(num + "  is not a prime number");
+                flag=1;
+                break;
+            }
+        }
+        } 
+        if(flag==0){
+            System.out.println(num + " is a prime number");
+        }
+
+    }
+
+
+
+    public static void copyArray() {
+        int [] arr1 = new int [] {1, 2, 3, 4, 5};
+        int arr2[] = new int[arr1.length];
+        
+        for (int i = 0; i < arr1.length; i++) {
+            arr2[i] = arr1[i];
+        }
+
+        System.out.println("Elements of original array: ");
+        for (int i = 0; i < arr1.length; i++) {
+            System.out.print(arr1[i] + " ");
+        }
+        
+        System.out.println();
+        
+        System.out.println("Elements of new array: ");
+        
+        for (int i = 0; i < arr2.length; i++) {
+            System.out.print(arr2[i] + " ");
+        
+        }
+    }
+       
+
+
+
+
     public static void findFactorial(){
 
         System.out.println("Enter Number to Find Factorial");
@@ -45,8 +99,11 @@ public class lab1Task {
         System.out.println("Reverse of Number is " + rev);
     }
 
+    
     public static void main(String args[]){
 
+        // primeNumber();
+        // copyArray();
         // findFactorial();
         reverseNumber();
 
